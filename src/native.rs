90e7a566c355d814f2821f8c7e15b5f0aef2532d
arnaucube <git@arnaucube.com>
2026-02-10 10:34:18 +0000

    org circuit's test_encrypt_block

--- src/native.rs
+++ src/native.rs
@@ -112,10 +112,7 @@ fn add_round_key(s: State, key: &[[u8; 4]]) -> State {
 
 pub(crate) fn key_expansion<const NK: usize, const NB: usize, const NR: usize>(
     key: &[u8; NK * NB],
-) -> [[u8; 4]; 4 * (NR + 1)]
-where
-    [(); 4 * (NR + 1)]:,
-{
+) -> [[u8; 4]; 4 * (NR + 1)] {
     let mut w = [[0u8; 4]; 4 * (NR + 1)]; // expanded key
 
     for i in 0..NK {
@@ -207,19 +204,16 @@ mod tests {
             0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37,
             0x07, 0x34,
         ];
-        let key = [
+        let key: [u8; 16] = [
             0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf,
             0x4f, 0x3c,
         ];
-        let expanded_key = key_expansion::<4, 4, 10>(&key);
-
-        let c = encrypt_block::<10>(&input, &expanded_key);
-
         let expected: [u8; 16] = [
             0x39, 0x02, 0xdc, 0x19, 0x25, 0xdc, 0x11, 0x6a, 0x84, 0x09, 0x85, 0x0b, 0x1d, 0xfb,
             0x97, 0x32,
         ];
-
+        let expanded_key = key_expansion::<4, 4, 10>(&key);
+        let c = encrypt_block::<10>(&input, &expanded_key);
         assert_eq!(flatten_state(c), expected);
     }
 
