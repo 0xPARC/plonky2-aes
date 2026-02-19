fdff3c5a468990c8b02af93b43d3da1e3f694a9b
Ahmad <root@ahmadafuni.com>
2026-02-20 05:52:08 +1000

    Add hashed ElGamal

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 - [aes-gcm](/aes-gcm): Plonky2 circuits for AES & AES-GCM encryption system as described at
 	- AES: https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.197-upd1.pdf
 	- AES-GCM (Galois Counter Mode): https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38d.pdf
+- [ecgfp5](/ecgfp5): Plonky2 circuits for ElGamal and (a finite field analogue of) hashed ElGamal cryptosystems.
 - [feistel](/feistel): a Plonky2 library for a finite field analogue of the Feistel cipher as described [here](https://en.wikipedia.org/wiki/Feistel_cipher).
--- ecgfp5/src/hashed_elgamal/circuit.rs
+++ ecgfp5/src/hashed_elgamal/circuit.rs
@@ -1,5 +1,9 @@
+use std::array;
+
 use plonky2::{
-    field::extension::Extendable, hash::hash_types::RichField,
+    field::extension::Extendable,
+    hash::{hash_types::RichField, poseidon::PoseidonHash},
+    iop::target::Target,
     plonk::circuit_builder::CircuitBuilder,
 };
 use pod2::backends::plonky2::{
@@ -12,27 +16,32 @@ use pod2::backends::plonky2::{
 
 use crate::circuit::PublicKeyTarget;
 
-pub trait CircuitBuilderElGamal<F: RichField + Extendable<D>, const D: usize> {
-    /// Encrypts an EcGFp5 point according to the ElGamal scheme.
-    fn elgamal_encrypt(
+pub trait CircuitBuilderHashedElGamal<F: RichField + Extendable<D>, const D: usize> {
+    /// Encrypts an EcGFp5 point according to the 'hashed ElGamal '
+    /// scheme described in `super::hashed_elgamal_encrypt`.
+    fn hashed_elgamal_encrypt(
         &mut self,
         pk: &PublicKeyTarget,
         nonce: &BigUInt320Target,
-        msg: &PointTarget,
-    ) -> (PointTarget, PointTarget);
+        msg: [Target; 5],
+    ) -> (PointTarget, [Target; 5]);
 }
 
-impl CircuitBuilderElGamal<F, D> for CircuitBuilder<F, D> {
-    fn elgamal_encrypt(
+impl CircuitBuilderHashedElGamal<F, D> for CircuitBuilder<F, D> {
+    fn hashed_elgamal_encrypt(
         &mut self,
         pk: &PublicKeyTarget,
         nonce: &BigUInt320Target,
-        msg: &PointTarget,
-    ) -> (PointTarget, PointTarget) {
+        msg: [Target; 5],
+    ) -> (PointTarget, [Target; 5]) {
         let generator = self.constant_point(Point::generator());
         let nonce_times_gen = self.multiply_point(&nonce.bits, &generator);
         let nonce_times_pk = self.multiply_point(&nonce.bits, pk);
-        (nonce_times_gen, self.add_point(msg, &nonce_times_pk))
+        let h = self.hash_n_to_m_no_pad::<PoseidonHash>(
+            [nonce_times_pk.x.components, nonce_times_pk.u.components].concat(),
+            5,
+        );
+        (nonce_times_gen, array::from_fn(|i| self.add(msg[i], h[i])))
     }
 }
 
@@ -40,7 +49,8 @@ impl CircuitBuilderElGamal<F, D> for CircuitBuilder<F, D> {
 mod tests {
     use num_bigint::RandBigInt;
     use plonky2::{
-        iop::witness::PartialWitness,
+        field::types::{Field, Field64},
+        iop::witness::{PartialWitness, WitnessWrite},
         plonk::{
             circuit_builder::CircuitBuilder, circuit_data::CircuitConfig,
             config::PoseidonGoldilocksConfig,
@@ -50,26 +60,26 @@ mod tests {
         basetypes::{D, F},
         primitives::ec::{
             bits::CircuitBuilderBits,
-            curve::{CircuitBuilderElliptic, GROUP_ORDER, Point, WitnessWriteCurve},
+            curve::{CircuitBuilderElliptic, GROUP_ORDER, WitnessWriteCurve},
         },
     };
-    use rand::rngs::OsRng;
+    use rand::{Rng, rngs::OsRng};
 
     use crate::{
         ECGFP5SecretKey,
-        elgamal::{circuit::CircuitBuilderElGamal, elgamal_encrypt},
+        hashed_elgamal::{circuit::CircuitBuilderHashedElGamal, hashed_elgamal_encrypt},
     };
 
     #[test]
-    fn elgamal_encryption() -> anyhow::Result<()> {
+    fn hashed_elgamal_encryption() -> anyhow::Result<()> {
         let config = CircuitConfig::standard_recursion_config();
         let mut builder = CircuitBuilder::<F, D>::new(config);
 
         let pk_target = builder.add_virtual_point_target();
         let nonce_target = builder.add_virtual_biguint320_target();
-        let msg_target = builder.add_virtual_point_target();
+        let msg_target = builder.add_virtual_target_arr();
 
-        let ct_target = builder.elgamal_encrypt(&pk_target, &nonce_target, &msg_target);
+        let ct_target = builder.hashed_elgamal_encrypt(&pk_target, &nonce_target, msg_target);
 
         let data = builder.build::<PoseidonGoldilocksConfig>();
 
@@ -77,17 +87,17 @@ mod tests {
             let sk = ECGFP5SecretKey::rand();
             let pk = sk.public_key();
 
-            let msg = Point::new_rand_from_subgroup();
+            let msg = std::array::from_fn(|_| F::from_canonical_u64(OsRng.gen_range(0..F::ORDER)));
             let nonce = OsRng.gen_biguint_below(&GROUP_ORDER);
 
-            let encrypted_msg = elgamal_encrypt(pk, &nonce, msg);
+            let encrypted_msg = hashed_elgamal_encrypt(pk, &nonce, msg);
 
             let mut pw = PartialWitness::new();
             pw.set_point_target(&pk_target, &pk)?;
-            pw.set_point_target(&msg_target, &msg)?;
+            pw.set_target_arr(&msg_target, &msg)?;
             pw.set_biguint320_target(&nonce_target, &nonce)?;
             pw.set_point_target(&ct_target.0, &encrypted_msg.0)?;
-            pw.set_point_target(&ct_target.1, &encrypted_msg.1)?;
+            pw.set_target_arr(&ct_target.1, &encrypted_msg.1)?;
 
             let proof = data.prove(pw)?;
             data.verify(proof)
